08b5759a7c04ab8333cb1adc00c176fc3bd531df
klccetin90-svg <klccetin90-svg@users.noreply.github.com>
2026-09-17 22:14:55 +0000

    deploy: 810e7b097cb464226d066d62a55eec8335b7cb25

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"06a2e2a110089dff50fe635cffd2a61e1b24fb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3147489526" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4292238080" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
